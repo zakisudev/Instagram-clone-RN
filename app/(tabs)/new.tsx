160fd471e356ce04a1a8c6aeb0e9a439cd069998
zakisudev <zakisudev@gmail.com>
2024-08-30 11:49:09 +0300

    chore: Add expo-image-picker plugin and update app.json and package.json

--- app/(tabs)/new.tsx
+++ app/(tabs)/new.tsx
@@ -1,20 +1,39 @@
 import { View, Text, Image, TextInput, Pressable } from 'react-native';
 import React, { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 const CreatePost = () => {
   const [caption, setCaption] = useState('');
+  const [image, setImage] = useState(
+    'https://facesconsent.com/images/default-product-image.png'
+  );
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [3, 4],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   return (
     <View className="flex-1 bg-white w-full">
       <Image
-        source={{
-          uri: 'https://facesconsent.com/images/default-product-image.png',
-        }}
+        source={{ uri: image }}
         className="w-full h-96 shadow-lg"
-        resizeMode="cover"
+        resizeMode="contain"
       />
 
-      <Text className="text-xl px-3 py-3 my-2 text-center text-cyan-800 w-48 mx-auto bg-gray-200 rounded">
+      <Text
+        className="text-xl px-3 py-3 my-2 text-center text-cyan-800 w-48 mx-auto bg-gray-200 rounded"
+        onPress={pickImage}
+      >
         Change Image
       </Text>
       <View className="items-center gap-3 w-full px-3">

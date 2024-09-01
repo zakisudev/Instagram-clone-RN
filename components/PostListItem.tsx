edee286bd0450c0404d9468c5a7cad8281985aad
zakisudev <zakisudev@gmail.com>
2024-09-01 10:46:13 +0300

    chore: Update PostListItem component to use Cloudinary for image rendering

--- components/PostListItem.tsx
+++ components/PostListItem.tsx
@@ -1,22 +1,36 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, useWindowDimensions, FlatList } from 'react-native';
 import React from 'react';
 import { AntDesign, Ionicons, Feather } from '@expo/vector-icons';
+import posts from '@/assets/data/posts.json';
+import { cld } from '../lib/cloudinary';
+import { AdvancedImage } from 'cloudinary-react-native';
+import { thumbnail } from '@cloudinary/url-gen/actions/resize';
+import { FocusOn } from '@cloudinary/url-gen/qualifiers/focusOn';
+import { focusOn } from '@cloudinary/url-gen/qualifiers/gravity';
 
-const PostListItem = ({ post }: any) => {
+const RenderItem = ({ post }: any) => {
+  const { width } = useWindowDimensions();
+
+  const image = cld.image(post?.image);
+  image.resize(thumbnail().width(width).height(width));
+
+  const avatar = cld.image(post?.user.avatar_url);
+  avatar.resize(
+    thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face()))
+  );
+
   return (
     <>
       <View className="w-full m-2 flex-row gap-4 items-center">
-        <Image
-          source={{ uri: post.user.image_url }}
-          className="w-16 aspect-square rounded-full"
+        <AdvancedImage
+          cldImg={avatar}
+          style={{ width: 54, height: 54, borderRadius: 50 }}
         />
-        <Text className="text-xl font-semibold">{post.user.username}</Text>
+        <Text className="text-xl font-semibold">{post?.user.username}</Text>
       </View>
-      <Image
-        source={{ uri: post.image_url }}
-        className="w-full aspect-[4/3]"
-        resizeMode="cover"
-      />
+
+      <AdvancedImage cldImg={image} style={{ width: '100%', height: 200 }} />
+
       <View className="p-3 w-full">
         <View className="flex-row gap-4 my-2">
           <AntDesign name="hearto" size={24} color="black" />
@@ -31,12 +45,26 @@ const PostListItem = ({ post }: any) => {
           />
         </View>
         <Text className="text-lg font-semibold">
-          {post.caption.length > 20
-            ? `${post.caption.slice(0, 45)}...`
-            : post.caption}
+          {post?.caption.length > 20
+            ? `${post?.caption.slice(0, 45)}...`
+            : post?.caption}
         </Text>
       </View>
     </>
+  );
+};
+
+const PostListItem = () => {
+  return (
+    <View className="w-full">
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <RenderItem post={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerClassName="gap-3"
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
   );
 };
 

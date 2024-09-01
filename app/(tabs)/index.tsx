9d5e275c80e779ff9b9b6feb0d904bbfcf6f5a0c
zakisudev <zakisudev@gmail.com>
2024-09-01 12:28:55 +0300

    refactor: Update FeedScreen to fetch and display posts from Supabase
    This commit refactors the FeedScreen component to fetch and display posts from Supabase. It adds a useEffect hook to fetch the posts on component mount and updates the PostListItem component to accept the fetched posts as props. The posts are now fetched from the 'posts' table in Supabase and reversed to display the most recent posts first. If there is an error fetching the posts, an alert is shown to the user.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,31 @@
-import { View } from 'react-native';
-import React from 'react';
+import { Alert, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import PostListItem from '@/components/PostListItem';
+import { supabase } from '@/lib/supabase';
+import { router } from 'expo-router';
 
 const FeedScreen = () => {
+  const [posts, setPosts] = useState<any[]>([]);
+
+  const fetchPosts = async () => {
+    let { data, error } = await supabase
+      .from('posts')
+      .select('*, user:profiles(*)');
+
+    if (error) {
+      Alert.alert('Something went wrong');
+    }
+
+    setPosts(data?.reverse() || []);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, [posts]);
+
   return (
     <View className="bg-white items-center w-full">
-      <PostListItem />
+      <PostListItem posts={posts} />
     </View>
   );
 };
